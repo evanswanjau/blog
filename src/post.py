b578c547dd7d2906601a8babfe1da9fa872103de
Evans Wanjau <evanswanjau@gmail.com>
2021-06-10 14:28:20 +0300

    Rewrote Post Class
    - Added method to create unique ids for our posts

--- src/post.py
+++ src/post.py
@@ -1,29 +1,30 @@
 """
 This file contains the Post class that takes care of everything invlovled in post management
 """
+import random
+
+
 class Post:
     """
     This class creates, reads, updates, delete posts;
     """
-    def __init__(self, post_id, title, post, data):
-        self.post_id = post_id
-        self.title = title
-        self.post = post
+
+    def __init__(self, data):
         self.data = data
 
-    def create_post(self):
+    def create_post(self, title, post):
         """
         Take in (post_id, title, post)
         and returns a single post.
         """
         new_post = {
-            'id':self.post_id,
-            'title':self.title,
-            'post':self.post,
+            "id": self.generate_post_id(),
+            "title": title,
+            "post": post,
         }
 
         self.data.append(new_post)
-        return new_post
+        return self.data
 
     def view_all_posts(self):
         """
@@ -36,26 +37,41 @@ class Post:
         Takes in post_id and returns post associated with post_id.
         """
         for i in self.data:
-            if i['id'] == post_id:
+            if i["id"] == post_id:
                 return i
-        return 'Post not found'
+        return "Post not found"
 
     def update_post(self, post_id, title, post):
         """
         Takes in post_id and updates post associated with post_id and returns updated post.
         """
         for i in self.data:
-            if i['id'] == post_id:
-                i['title'] = title
-                i['post'] = post
+            if i["id"] == post_id:
+                i["title"] = title
+                i["post"] = post
                 return i
-        return 'Post not found'
+        return "Post not found"
 
     def delete_post(self, post_id):
         """
         Takes in post_id and deletes post associated with post_id returns deleted post.
         """
         for i in range(len(self.data)):
-            if self.data[i]['id'] == post_id:
+            if self.data[i]["id"] == post_id:
                 return self.data.pop(i)
-        return 'Post not found'
+        return "Post not found"
+
+    def generate_post_id(self):
+        """
+        This recursive method allows us to create automated unique post ids
+        Returns 10 character string
+        """
+        string = list("abcdefghijklmnopqrstuvwxyz0123456789")
+        random.shuffle(string)
+        string = "".join(string)
+
+        # ensure post id doesn't exits in the db
+        for i in self.data:
+            if i["id"] == string:
+                self.generate_post_id()
+        return string[:10]
