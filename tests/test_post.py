43831f9780c5c904696181d1844739e2edccbf99
Evans Wanjau <evanswanjau@gmail.com>
2021-06-10 14:41:59 +0300

    Updated tests to fi new post update

--- tests/test_post.py
+++ tests/test_post.py
@@ -1,20 +1,27 @@
 """
 Tests to take care of our posts
 """
-import pytest
 from ..src.post import Post
 
-test_data = [{"post": "This is a new post", "id": 30, "title": "New Post"}]
+test_data = [
+    {"post": "This is a new post", "id": 30, "title": "New Post"},
+    {"post": "Welcome to deadwood", "id": 31, "title": "Deadwood series"},
+]
 
-test_post = Post(31, "Test Title", "This is a test post for our unit test", test_data)
+test_post = Post(test_data)
+
 
 def test_create_post():
-        """
-        Function to test our create post
-        Must return an object
-        """
-        created_data = test_post.create_post()
-        assert created_data["title"] == "Test Title"
+    """
+    Function to test our create post
+    Must return an object
+    """
+    created_data = test_post.create_post(
+        "Test Title", "This is a test post for our unit test"
+    )
+    created_data = created_data[len(created_data) - 1]
+    assert created_data["title"] == "Test Title"
+
 
 def test_view_single_post():
     """
@@ -22,6 +29,7 @@ def test_view_single_post():
     """
     single_post_data = test_post.view_single_post(30)
     assert single_post_data["title"] == "New Post"
+
 
 def test_update_post():
     """
@@ -36,6 +44,7 @@ def test_update_post():
 
     assert successful_update["title"] == "Newer Post"
     assert failed_update == "Post not found"
+
 
 def test_delete_post():
     """
